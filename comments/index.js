0f258b1b9496ccbc26972aebba50034d042fbba1
yamen <yamen225@gmail.com>
2021-01-11 21:41:50 +0200

    initial commmit with micro services comments posts and client using react

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const { randomBytes } = require( 'crypto' );
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const cors = require( 'cors' )
+
+const app = express();
+app.use( bodyParser.json() );
+app.use( cors() );
+
+const commentsByPostId = {};
+
+app.get( '/posts/:id/comments', ( req, res ) => {
+    res.send( commentsByPostId[req.params.id] || [] )
+} );
+
+app.post( '/posts/:id/comments', ( req, res ) => {
+    const commmentId = randomBytes( 4 ).toString( 'hex' );
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || []
+
+    comments.push( { id: commmentId, content } )
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status( 201 ).send( comments )
+} );
+
+
+app.listen( 4001, () => {
+    console.log( 'listening on 4001' );
+} );
