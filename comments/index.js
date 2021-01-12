c2903be4082a64664a04bf6e8fdcce8207bdad88
yamen <yamen225@gmail.com>
2021-01-12 17:10:58 +0200

    create minimal event broker service, post service and comment service are emitting events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const { randomBytes } = require( 'crypto' );
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const cors = require( 'cors' )
+const axios = require( 'axios' )
 
 const app = express();
 app.use( bodyParser.json() );
@@ -13,7 +14,7 @@ app.get( '/posts/:id/comments', ( req, res ) => {
     res.send( commentsByPostId[req.params.id] || [] )
 } );
 
-app.post( '/posts/:id/comments', ( req, res ) => {
+app.post( '/posts/:id/comments', async ( req, res ) => {
     const commmentId = randomBytes( 4 ).toString( 'hex' );
     const { content } = req.body;
 
@@ -23,6 +24,12 @@ app.post( '/posts/:id/comments', ( req, res ) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post( 'http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commmentId, content, postId: req.params.id
+        }
+    } )
     res.status( 201 ).send( comments )
 } );
 
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const { randomBytes } = require( 'crypto' );
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const cors = require( 'cors' );
+const axios = require( 'axios' )
 
 const app = express();
 app.use( bodyParser.json() );
@@ -13,11 +14,15 @@ app.get( '/posts', ( req, res ) => {
     res.send( posts );
 } );
 
-app.post( '/posts', ( req, res ) => {
+app.post( '/posts', async ( req, res ) => {
     const id = randomBytes( 4 ).toString( 'hex' )
     const { title } = req.body;
-    console.log(title);
+    console.log( title );
     posts[id] = { id, title };
+    await axios.post( 'http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: { id, title }
+    } );
     res.status( 201 ).send( posts[id] );
 } );
 
