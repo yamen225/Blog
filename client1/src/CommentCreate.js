24c328333cb1d6915261d33460f9d250f9e7569d
yamen <yamen225@gmail.com>
2021-01-17 00:30:59 +0200

    create deploment for client service and fix non unique routes

--- client1/src/CommentCreate.js
+++ client1/src/CommentCreate.js
@@ -7,7 +7,7 @@ export default ( { postId } ) => {
   const onSubmit = async ( event ) => {
     event.preventDefault();
     console.log( content );
-    await axios.post( `http://localhost:4001/posts/${postId}/comments`,
+    await axios.post( `http://posts.com/posts/${postId}/comments`,
       { content } );
     setContent( '' );
   }
